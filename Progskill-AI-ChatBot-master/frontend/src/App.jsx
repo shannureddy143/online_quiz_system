8d918c6d308791b708e3aab04e8fa397df650ed6
shannureddy143 <shannureddy369@gmail.com>
2025-11-06 23:00:55 +0530

    Add files via upload

---
+++ Progskill-AI-ChatBot-master/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chat from './components/Chat'
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-gradient-to-br from-blue-50 via-white to-blue-50">
+      <Chat />
+    </div>
+  )
+}
+
+export default App
